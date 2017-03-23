faeec3773e46f8d3e6ab354fe8eb4cffe4d5ef6f
Deepak Kadarivel <deepak@Deepaks-MacBook-Pro.local>
2017-03-23 14:06:26 +0530

    adds _01_02Variables shorthand

---
+++ _02Variables/shorthand/type/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	a := 1
+	b := 100.239
+	c := true
+	d := "I'm a designer"
+	e := 'd'
+	f := `Stream`
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%T \n", e)
+	fmt.Printf("%T \n", f)
+
+}
---
+++ _02Variables/shorthand/values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	a := 1
+	b := 100.20
+	c := false
+	d := "I'm a dev"
+	e := '$'
+	f := `Applied`
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+
+}
